612e2aec80b7e75131d98ff9d1d77f41a30fb736
Scott Haney <emailalmondbranch@gmail.com>
2021-12-18 12:05:57 -0500

    Add a test for a simple class and get it to fail

--- Maestro.Core.Tests/BigClassHelperTests.cs
+++ Maestro.Core.Tests/BigClassHelperTests.cs
@@ -18,5 +18,19 @@ namespace Maestro.Core.Tests
                 Assert.IsTrue(result.IsEmpty);
             }
         }
+
+        [Test]
+        public void Finds_References_To_A_Field_Used_In_A_Method()
+        {
+            var emptyClass = @"public class Test { public readonly int Field; public void TestMethod() { return Field + 1; }}";
+
+            using (var mock = AutoMock.GetLoose())
+            {
+                var instance = mock.Create<BigClassHelper>();
+
+                var result = instance.CreateDiagram(emptyClass);
+                Assert.IsFalse(result.IsEmpty);
+            }
+        }
     }
 }

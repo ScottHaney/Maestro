f603859bcd01f87bd181ef856e9912b8ab43aa96
Scott Haney <emailalmondbranch@gmail.com>
2021-12-18 12:12:29 -0500

    Add in a place to store variableReferences

--- Maestro.Core/BigClassHelper.cs
+++ Maestro.Core/BigClassHelper.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Maestro.Core
 {
@@ -11,12 +14,34 @@ namespace Maestro.Core
             var tree = CSharpSyntaxTree.ParseText(csFileWithClass);
             var root = tree.GetCompilationUnitRoot();
 
-            return new InternalClassDiagram();
+            return new InternalClassDiagram(GetVariableNodes(root).ToList());
+        }
+
+        private IEnumerable<VariableNode> GetVariableNodes(CompilationUnitSyntax root)
+        {
+            return Enumerable.Empty<VariableNode>();
         }
     }
 
     public class InternalClassDiagram
     {
-        public bool IsEmpty => true;
+        public readonly List<VariableNode> VariableNodes;
+
+        public bool IsEmpty => !VariableNodes.Any();
+
+        public InternalClassDiagram(List<VariableNode> variableNodes)
+        {
+            VariableNodes = variableNodes;
+        }
+    }
+
+    public class VariableNode
+    {
+        public readonly string Name;
+        
+        public VariableNode(string name)
+        {
+            Name = name;
+        }
     }
 }

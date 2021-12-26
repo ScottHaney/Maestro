3208cd86624084850a734458155b13aa5f83a8e4
Scott Haney <emailalmondbranch@gmail.com>
2021-12-26 17:37:15 -0500

    Add the outline for finding connected components

---
+++ Maestro.Core/ConnectedComponentsFinder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maestro.Core
+{
+    public class ConnectedComponentsFinder
+    {
+        public List<ConnectedComponent> Find(InternalClassDiagram diagram)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DFS(InternalClassDiagram diagram)
+        {
+
+        }
+    }
+
+    public class ConnectedComponent
+    {
+        public readonly List<FunctionNode> Nodes;
+
+        public ConnectedComponent(List<FunctionNode> nodes)
+        {
+            Nodes = nodes;
+        }
+    }
+}

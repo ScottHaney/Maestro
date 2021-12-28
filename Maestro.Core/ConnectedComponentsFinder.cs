ff55197f717b91981053c88179b3daf8e2177d79
Scott Haney <emailalmondbranch@gmail.com>
2021-12-28 18:29:57 -0500

    Put in a basic implementation for the Find method

--- Maestro.Core/ConnectedComponentsFinder.cs
+++ Maestro.Core/ConnectedComponentsFinder.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Maestro.Core
@@ -8,7 +9,19 @@ namespace Maestro.Core
     {
         public List<GraphComponent> Find(InternalClassGraph graph)
         {
-            throw new NotImplementedException();
+            var result = new List<GraphComponent>();
+
+            var allVisitedNodes = new HashSet<InternalClassNode>();
+            while (allVisitedNodes.Count < graph.Nodes.Count)
+            {
+                var nextVisitedSet = new HashSet<InternalClassNode>();
+                DFS(graph, graph.Nodes.First(x => !allVisitedNodes.Contains(x)), nextVisitedSet);
+
+                result.Add(new GraphComponent(nextVisitedSet.ToList()));
+                allVisitedNodes.UnionWith(nextVisitedSet);
+            }
+
+            return result;
         }
 
         private void DFS(InternalClassGraph graph, InternalClassNode startNode, HashSet<InternalClassNode> visitedNodes)

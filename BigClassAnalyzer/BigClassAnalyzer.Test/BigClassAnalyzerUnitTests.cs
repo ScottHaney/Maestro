fa8859cffc50ee8e2eabebbdc71b5746670b15c3
Scott Haney <emailalmondbranch@gmail.com>
2022-01-03 08:52:11 -0500

    Get the analyzer unit test to pass for an example big class

--- BigClassAnalyzer/BigClassAnalyzer.Test/BigClassAnalyzerUnitTests.cs
+++ BigClassAnalyzer/BigClassAnalyzer.Test/BigClassAnalyzerUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 using VerifyCS = BigClassAnalyzer.Test.CSharpCodeFixVerifier<
     BigClassAnalyzer.BigClassAnalyzerAnalyzer,
@@ -11,7 +12,7 @@ namespace BigClassAnalyzer.Test
     {
         //No diagnostics expected to show up
         [TestMethod]
-        public async Task TestMethod1()
+        public async Task Empty_Text_Produces_Nothing()
         {
             var test = @"";
 
@@ -20,24 +21,9 @@ namespace BigClassAnalyzer.Test
 
         //Diagnostic and CodeFix both triggered and checked for
         [TestMethod]
-        public async Task TestMethod2()
+        public async Task Class_That_Has_Two_Connected_Components_Produces_A_Warning()
         {
-            var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class {|#0:TypeName|}
-        {   
-        }
-    }";
-
-            var fixtest = @"
+            var csharpCode = @"
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -60,8 +46,33 @@ namespace BigClassAnalyzer.Test
         }
     }";
 
-            var expected = VerifyCS.Diagnostic("BigClassAnalyzer").WithLocation(0).WithArguments("BigClass");
-            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);
+            var actual = VerifyCS.Diagnostic("BigClassAnalyzer").WithLocation(0).WithArguments("BigClass");
+            Assert.AreNotEqual(DiagnosticResult.EmptyDiagnosticResults, actual);
+
+            /*var fixtest = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class BigClass
+        {
+            private readonly int field1;
+            private readonly int field2;
+
+            public int Method1()
+                => field1 + 1;
+
+            public int Method2()
+                => field2 + 1;
+        }
+    }";
+
+            await VerifyCS.VerifyCodeFixAsync(test, expected, fixtest);*/
         }
     }
 }

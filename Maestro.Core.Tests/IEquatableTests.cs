1960d7489f4c9ea841df904a7f148d3ab48abc0f
Scott Haney <emailalmondbranch@gmail.com>
2022-01-14 05:49:03 -0700

    Add a method for equals operation checks

--- Maestro.Core.Tests/IEquatableTests.cs
+++ Maestro.Core.Tests/IEquatableTests.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 
 namespace Maestro.Core.Tests
 {
@@ -15,15 +16,26 @@ namespace Maestro.Core.Tests
             var node1IdenticalTwin = new InternalClassNode("node", InternalClassNodeType.Function);
             var node2 = new InternalClassNode("node", InternalClassNodeType.Variable);
 
-            Assert.IsTrue(node1.Equals(node1IdenticalTwin));
-            Assert.IsTrue(node1.Equals((object)node1IdenticalTwin));
-            Assert.IsTrue(node1 == node1IdenticalTwin);
+            RunEqualsChecks(node1, node1IdenticalTwin);
 
             Assert.IsFalse(node1.Equals(node2));
             Assert.IsFalse(node1.Equals((object)node2));
             Assert.IsTrue(node1 != node2);
 
             RunGetHashCodeChecks(node1, node1IdenticalTwin, node2);
+        }
+
+        private void RunEqualsChecks<T>(T lhs, T rhs)
+            where T : IEquatable<T>
+        {
+            Assert.IsTrue(lhs.Equals(rhs));
+            Assert.IsTrue(lhs.Equals((object)rhs));
+
+            var equalsOperator = typeof(T).GetMethod("op_Equality", BindingFlags.Static | BindingFlags.Public);
+            if (equalsOperator == null)
+                throw new Exception("The equals operator was not implemented. Make sure to implement this to get consistent behavior");
+
+            Assert.IsTrue((bool)equalsOperator.Invoke(null, new object[] { lhs, rhs }));
         }
 
         private void RunGetHashCodeChecks<T>(params T[] items)

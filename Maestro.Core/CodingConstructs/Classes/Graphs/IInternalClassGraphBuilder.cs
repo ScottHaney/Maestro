308c33a3a68aac734f9385c71bdfa6d01385055d
Scott Haney <emailalmondbranch@gmail.com>
2022-03-04 16:20:20 -0500

    Add in the new builder

---
+++ Maestro.Core/CodingConstructs/Classes/Graphs/IInternalClassGraphBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Maestro.Core.CodingConstructs.Classes.Architecture;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maestro.Core.CodingConstructs.Classes.Graphs
+{
+    public interface IInternalClassGraphBuilder
+    {
+        void AddNode(VariableNode node);
+        void AddNode(MethodNode node);
+        IInternalClassGraph Build();
+    }
+}
---
+++ Maestro.Core/CodingConstructs/Classes/Graphs/InternalClassGraphBuilder.cs
@@ -0,0 +1,27 @@
+﻿using Maestro.Core.CodingConstructs.Classes.Architecture;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maestro.Core.CodingConstructs.Classes.Graphs
+{
+    public class InternalClassGraphBuilder : IInternalClassGraphBuilder
+    {
+        private readonly List<Node> _nodes = new List<Node>();
+
+        public void AddNode(VariableNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddNode(MethodNode node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IInternalClassGraph Build()
+        {
+            return new InternalClassGraph(_nodes);
+        }
+    }
+}

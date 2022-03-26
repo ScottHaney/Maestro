e63b91656457b07d49cef4f3cdfb60f6a494167c
Scott Haney <emailalmondbranch@gmail.com>
2022-03-26 14:38:43 -0400

    Remove the unused code fix method

--- BigClassAnalyzer/BigClassAnalyzer.CodeFixes/BigClassAnalyzerCodeFixProvider.cs
+++ BigClassAnalyzer/BigClassAnalyzer.CodeFixes/BigClassAnalyzerCodeFixProvider.cs
@@ -141,26 +141,5 @@ namespace BigClassAnalyzer
 
             
         }*/
-
-        
-
-        private async Task<Solution> MakeUppercaseAsync(Document document, TypeDeclarationSyntax typeDecl, CancellationToken cancellationToken)
-        {
-            // Compute new uppercase name.
-            var identifierToken = typeDecl.Identifier;
-            var newName = identifierToken.Text.ToUpperInvariant();
-
-            // Get the symbol representing the type to be renamed.
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-            var typeSymbol = semanticModel.GetDeclaredSymbol(typeDecl, cancellationToken);
-
-            // Produce a new solution that has all references to that type renamed, including the declaration.
-            var originalSolution = document.Project.Solution;
-            var optionSet = originalSolution.Workspace.Options;
-            var newSolution = await Renamer.RenameSymbolAsync(document.Project.Solution, typeSymbol, newName, optionSet, cancellationToken).ConfigureAwait(false);
-
-            // Return the new solution with the now-uppercase type name.
-            return newSolution;
-        }
     }
 }

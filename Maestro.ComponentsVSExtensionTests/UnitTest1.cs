cd210dfe8a10e871b0c9cc48cf04fd3456d2fd51
Scott Haney <emailalmondbranch@gmail.com>
2022-03-31 20:54:36 -0400

    Add in a unit test project for the components extension

---
+++ Maestro.ComponentsVSExtensionTests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using NUnit.Framework;
+
+namespace Maestro.ComponentsVSExtensionTests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+        }
+    }
+}

d5c81dcc5ad57b556dc55903e9bcb1caa5926fd8
Scott Haney <emailalmondbranch@gmail.com>
2022-04-08 19:29:26 -0400

    Add interfaces for the component registry

---
+++ Maestro.Core/Components/IComponentRegistry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maestro.Core.Components
+{
+    public interface IComponent
+    {
+        string Name { get; }
+    }
+
+    public interface IComponentRegistry
+    {
+    }
+
+    public interface IComponentRegistrySource
+    {
+        IEnumerable<IComponent> GetComponents();
+    }
+}

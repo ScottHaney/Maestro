bd04761a9a51a0ca5fb72f942201349146269298
Scott Haney <emailalmondbranch@gmail.com>
2022-04-08 20:05:01 -0400

    Get a reference to the visual studio workspace for roslyn

--- ComponentsVSExtension/ComponentsVSExtensionPackage.cs
+++ ComponentsVSExtension/ComponentsVSExtensionPackage.cs
@@ -2,6 +2,9 @@
 global using Microsoft.VisualStudio.Shell;
 global using System;
 global using Task = System.Threading.Tasks.Task;
+using Microsoft.VisualStudio.ComponentModelHost;
+using Microsoft.VisualStudio.LanguageServices;
+using System.ComponentModel.Composition;
 using System.Runtime.InteropServices;
 using System.Threading;
 
@@ -14,9 +17,14 @@ namespace ComponentsVSExtension
     [Guid(PackageGuids.ComponentsVSExtensionString)]
     public sealed class ComponentsVSExtensionPackage : ToolkitPackage
     {
+        public static VisualStudioWorkspace CurrentWorkspace { get; set; }
+
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             await this.RegisterCommandsAsync();
+
+            var componentModel =  (IComponentModel)this.GetService(typeof(SComponentModel));
+            CurrentWorkspace = componentModel.GetService<VisualStudioWorkspace>();
 
             this.RegisterToolWindows();
         }

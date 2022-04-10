9d16cc24fab9e91205faf07ebe137d738424e22c
Scott Haney <emailalmondbranch@gmail.com>
2022-04-10 10:59:31 -0400

    Remove the rest of the diagnostic id warnings

--- BigClassAnalyzer/BigClassAnalyzer/AnalyzerReleases.Unshipped.md
+++ BigClassAnalyzer/BigClassAnalyzer/AnalyzerReleases.Unshipped.md
@@ -1,3 +1,9 @@
 ﻿; Unshipped analyzer release
 ; https://github.com/dotnet/roslyn-analyzers/blob/main/src/Microsoft.CodeAnalysis.Analyzers/ReleaseTrackingAnalyzers.Help.md
 
+### New Rules
+
+Rule ID | Category | Severity | Notes
+--------|----------|----------|-------
+Analyzes classes to see if they are composed of smaller units. | Naming | Warning | BigClassAnalyzerAnalyzer
+BigClassAnalyzer | Naming | Warning | BigClassAnalyzerAnalyzer
--- BigClassAnalyzer/BigClassAnalyzer/Resources.Designer.cs
+++ BigClassAnalyzer/BigClassAnalyzer/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace BigClassAnalyzer {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -61,7 +61,7 @@ namespace BigClassAnalyzer {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Finds classes that are really more than one class and breaks them into individual classes.
+        ///   Looks up a localized string similar to Analyzes classes to see if they are composed of smaller units..
         /// </summary>
         internal static string AnalyzerDescription {
             get {

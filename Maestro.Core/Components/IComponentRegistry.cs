16d2a6dae104975d9348079e507697dcb00b7657
Scott Haney <emailalmondbranch@gmail.com>
2022-04-16 21:24:50 -0400

    Add a WorkspaceComponentRegistry

--- Maestro.Core/Components/IComponentRegistry.cs
+++ Maestro.Core/Components/IComponentRegistry.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Maestro.Core.Components
 {
@@ -36,6 +37,36 @@ namespace Maestro.Core.Components
         public static void ExtractMethod(int startLine, int endLine, SemanticModel semanticModel)
         {
             var syntaxTree = semanticModel.SyntaxTree;
+        }
+    }
+
+    public class WorkspaceComponentRegistry : IComponentRegistry
+    {
+        private readonly Workspace _workspace;
+
+        public WorkspaceComponentRegistry(Workspace workspace)
+        {
+            _workspace = workspace;
+        }
+
+        public async Task<IEnumerable<ICodeComponent>> GetComponentsAsync()
+        {
+            var results = new List<ICodeComponent>();
+            foreach (var project in _workspace.CurrentSolution.Projects)
+            {
+                foreach (var document in project.Documents.Where(x => x.SupportsSyntaxTree))
+                {
+                    var syntaxTree = await document.GetSyntaxTreeAsync();
+                    results.AddRange(RoslynHelpers.GetComponents(syntaxTree));
+                }
+            }
+
+            return results;
+        }
+
+        private IEnumerable<ICodeComponent> GetComponents(SyntaxTree syntaxTree)
+        {
+            return RoslynHelpers.GetComponents(syntaxTree);
         }
     }
 

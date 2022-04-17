67c7dec412e53d4c25bef2d84f25becfa2fa1c7e
Scott Haney <emailalmondbranch@gmail.com>
2022-04-17 09:00:56 -0400

    Add in a second test for the workspace registry

---
+++ Maestro.Core.Tests/TestCsFiles/WorkspaceComponentRegistryTestFileThatDoesNotCompile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maestro.Core.Tests.TestCsFiles
+{
+    public class Component1
+    {
+    }
+
+    dfadf
+
+    public class Component2
+    {
+    }
+}
--- Maestro.Core.Tests/WorkspaceComponentRegistryTests.cs
+++ Maestro.Core.Tests/WorkspaceComponentRegistryTests.cs
@@ -11,14 +11,25 @@ namespace Maestro.Core.Tests
     public class WorkspaceComponentRegistryTests
     {
         [Test]
-        public async Task Gets_Classes_From_Single_File_With_No_Build_Errors()
+        public async Task Gets_Components_From_Single_File_With_No_Build_Errors()
         {
             var workspace = TestUtils.CreateSingleDocumentWorkspace(@"TestCsFiles/WorkspaceComponentRegistryTestFileThatCompiles.cs");
             var registry = new WorkspaceComponentRegistry(workspace);
 
-            var components = (await registry.GetComponentsAsync()).ToList();
+            var components = await registry.GetComponentsAsync();
 
             CollectionAssert.AreEquivalent(new[] { "Component1", "Component2", "IComponent" }, components.Select(x => x.Name));
+        }
+
+        [Test]
+        public async Task Gets_Components_From_Single_File_With_Build_Errors()
+        {
+            var workspace = TestUtils.CreateSingleDocumentWorkspace(@"TestCsFiles/WorkspaceComponentRegistryTestFileThatDoesNotCompile.cs");
+            var registry = new WorkspaceComponentRegistry(workspace);
+
+            var components = await registry.GetComponentsAsync();
+
+            CollectionAssert.AreEquivalent(new[] { "Component1", "Component2" }, components.Select(x => x.Name));
         }
     }
 }

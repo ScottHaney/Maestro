dc76ad7f0ccfbdcf7544a07edee43570f6aabcb7
Scott Haney <scotthaney323@gmail.com>
2022-04-22 22:59:25 -0400

    Add in a test for the workspace watcher, although it doesn't work just yet

---
+++ Maestro.Core/WorkspaceWatcher.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maestro.Core
+{
+    public class WorkspaceWatcher
+    {
+        private readonly Workspace _workspace;
+
+        public event EventHandler ComponentRenamed;
+
+        public WorkspaceWatcher(Workspace workspace)
+        {
+            _workspace = workspace;
+
+            _workspace.WorkspaceChanged += _workspace_WorkspaceChanged;
+        }
+
+        private void _workspace_WorkspaceChanged(object sender, WorkspaceChangeEventArgs e)
+        {
+
+        }
+    }
+}

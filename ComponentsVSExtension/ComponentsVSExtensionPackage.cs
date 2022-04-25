f78ba76b9405fdf1175923cff8b10cc2c61e08ab
Scott Haney <scotthaney323@gmail.com>
2022-04-24 23:12:43 -0400

    Add in the outline for the tags visual studio extension

--- ComponentsVSExtension/ComponentsVSExtensionPackage.cs
+++ ComponentsVSExtension/ComponentsVSExtensionPackage.cs
@@ -2,31 +2,69 @@
 global using Microsoft.VisualStudio.Shell;
 global using System;
 global using Task = System.Threading.Tasks.Task;
-using Microsoft.VisualStudio.ComponentModelHost;
-using Microsoft.VisualStudio.LanguageServices;
-using System.ComponentModel.Composition;
+using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Linq;
+using Microsoft.VisualStudio;
 
 namespace ComponentsVSExtension
 {
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
-    [ProvideToolWindow(typeof(MyToolWindow.Pane), Style = VsDockStyle.Tabbed, Window = WindowGuids.SolutionExplorer)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.NoSolution_string, PackageAutoLoadFlags.BackgroundLoad)]
     [Guid(PackageGuids.ComponentsVSExtensionString)]
     public sealed class ComponentsVSExtensionPackage : ToolkitPackage
     {
-        public static VisualStudioWorkspace CurrentWorkspace { get; set; }
-
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
-            await this.RegisterCommandsAsync();
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
 
-            var componentModel =  (IComponentModel)(await this.GetServiceAsync(typeof(SComponentModel)));
-            CurrentWorkspace = componentModel.GetService<VisualStudioWorkspace>();
+            if (await GetServiceAsync(typeof(IMenuCommandService)) is OleMenuCommandService mcs)
+            {
+                CommandID menuCommandID = new CommandID(PackageGuids.ComponentsVSExtension, PackageIds.AddTagsCommand);
+                OleMenuCommand menuItem = new OleMenuCommand(ExecuteAsync, menuCommandID);
 
-            this.RegisterToolWindows();
+                menuItem.BeforeQueryStatus += MenuItem_BeforeQueryStatus;
+
+                mcs.AddCommand(menuItem);
+            }
+        }
+
+        private async void MenuItem_BeforeQueryStatus(object sender, EventArgs e)
+        {
+            var menuCommand = (OleMenuCommand)sender;
+
+            var isVisible = false;
+            var solutionExplorer = await VS.Windows.GetSolutionExplorerWindowAsync();
+            if (solutionExplorer != null)
+            {
+                var selections = await solutionExplorer.GetSelectionAsync();
+
+                var hasFile = false;
+                var hasFolder = false;
+                foreach (var selection in selections)
+                {
+                    if (selection.Type == SolutionItemType.PhysicalFile)
+                        hasFile = true;
+                    else if (selection.Type == SolutionItemType.PhysicalFolder)
+                        hasFolder = true;
+
+                    if (hasFile && hasFolder)
+                        break;
+                }
+
+                isVisible = (hasFile && hasFolder);
+            }
+
+            menuCommand.Visible = isVisible;
+            menuCommand.Enabled = isVisible;
+        }
+
+        private async void ExecuteAsync(object sender, EventArgs e)
+        {
+
         }
     }
 }
--- ComponentsVSExtension/source.extension.cs
+++ ComponentsVSExtension/source.extension.cs
@@ -7,12 +7,12 @@ namespace ComponentsVSExtension
 {
     internal sealed partial class Vsix
     {
-        public const string Id = "ComponentsVSExtension.9a385a0d-b057-4d91-8122-a6449997ab9b";
+        public const string Id = "ComponentsVSExtension.bbd916ae-c9ee-4dfb-bbba-dbad292691eb";
         public const string Name = "ComponentsVSExtension";
         public const string Description = @"Empty VSIX Project.";
         public const string Language = "en-US";
         public const string Version = "1.0";
-        public const string Author = "Company";
+        public const string Author = "Scott Haney";
         public const string Tags = "";
     }
 }
